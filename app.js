110c8cb617607e2399321ec0a127d66b9813f588
imadghali01 <imadghali01@gmail.com>
2024-09-05 12:20:18 +0200

    exo1-part3

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
-alert("Hello there !");
-console.log("I am in the console");
+let name = prompt(`what's your name?`);
+let old = prompt(`what's your age`);;
+let livingCity = prompt(`what's your living city?`);;
+alert(`Hello, ${name} your age is ${old} years old and you live in ${livingCity}.`)
