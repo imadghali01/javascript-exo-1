0aaf730f898cda0e4ebbc624b107305072a0a481
imadghali01 <imadghali01@gmail.com>
2024-09-05 17:44:32 +0200

    try not finished

--- README.md
+++ README.md
@@ -1,22 +1,19 @@
 # javascript-exo-1
  begining js cursus
  
-- Create in a folder an `index.html` file and a `app.js` file.
+## part 8
 
-- Let's link the js file to your html !
+Create an array that should look like suche : `["one","two","three"]`
 
-  - In your HTML file, insert this code and save : `<script src="app.js"></script>`
+In order to do that, only do manipulations from this folowing array :
 
-- Then, in your `app.js` insert this line of code : ` alert("Hello there !");`
-
-- Now, go to your browser and check if there is a popup with `Hello there` that appears. If not, there is a problem. If yes, erase the alert in your code and let's keep going !
-
----
-
-- Now, instead of `alert("Hello there !");`, paste `console.log("I am in the console");`
-
-- In your browser, open the console (F12) or right click and select `inspect`.
-
-- Check the console : your text is there.
-
-- We will be using the console all the time to run and interact with JavaScript.
+const initialArray = [
+  null,
+  true,
+  ["Apple", "two", undefined],
+  false,
+  ["three", "BeCode"],
+  4,
+  "I am a big bad wolf",
+  ["one"],
+];
--- app.js
+++ app.js
@@ -1,2 +1,67 @@
-alert("Hello there !");
-console.log("I am in the console");
+/*Create an array that should look like suche : `["one","two","three"]`
+
+In order to do that, only do manipulations from this folowing array :
+- The methods :
+  - Pop and Push
+  - Shift and unshift
+  - Concat
+  - Join
+  - Split (which is a **STRING** method that transforms a string into an array)
+  - Slice
+  - Splice
+  - Reverse
+  - Sort
+  - Includes
+*/
+const newArray=[];
+const initialArray = [
+  null,
+  true,
+  ["Apple", "two", undefined],
+  false,
+  ["three", "BeCode"],
+  4,
+  "I am a big bad wolf",
+  ["one"],
+];
+
+for(let i=0 ; i=initialArray.length-1 ; i++){
+    if(typeof(initialArray[i])== "object"){
+        initialArray[i].forEach(element => {
+            if(element == "one"){
+                initialArray[0] = element;
+            }
+            else if(element == "two"){
+                initialArray[1] = element;
+            }
+            else if(element == "three"){
+                initialArray[2] = element;
+            }
+        });
+    }
+
+    else if(initialArray[i] == "one"){
+        newArray[0] = initialArray[i];
+    }
+    else if(initialArray[i] == "two"){
+        newArray[1] = initialArray[i];
+    }
+    else if(initialArray[i] == "three"){
+        newArray[2] = initialArray[i];
+    }
+    else{
+        arraySplicer(initialArray, i, 1)
+    }
+
+    
+    
+}
+
+
+
+
+function arraySplicer(array, x, y){
+    array.splice(x, y);
+}
+
+
