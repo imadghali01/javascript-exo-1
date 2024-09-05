9c0240eff2c8c02a99f47c56c295c57f33aed075
imadghali01 <imadghali01@gmail.com>
2024-09-05 12:53:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 let circleRadius = prompt(`give me a circle raduis i will return you the surface of it!`);
 let circleSurface = ((circleRadius**2) * Math.PI);
-alert(`the surface of a ${circleRadius} cm raduis circle is ${circleSurface.toFixed()}`);
+alert(`the surface of a ${circleRadius} cm radius circle is ${circleSurface.toFixed()}`);
