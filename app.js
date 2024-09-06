bba52ccd614b760460b07f9b25f539e094581d3a
imadghali01 <imadghali01@gmail.com>
2024-09-06 16:22:03 +0200

    exo1-13

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
-alert("Hello there !");
-console.log("I am in the console");
+
+let max = prompt("Please enter a max number :");
+while (isNaN(max) || max < 1) {
+    max = prompt("Please enter a valid max number :");
+}
+console.log(max);
+
+let secretNum = Math.floor(Math.random() * max);
+
+console.log(secretNum);
+
+let guess = prompt(`I have chosen a number between 1 and ${max}, can you guess it ?`);
+while (guess != secretNum) {
+    while (isNaN(guess) || max < 1) {
+        guess = prompt(`try again with a correct number?`);
+    } 
+    if (guess < secretNum) {
+        guess = prompt(`The secret number is higher than ${guess}, try again:`);
+    } 
+    else {
+        guess = prompt(`The secret number is lower than ${guess}, try again:`);
+    } 
+}
+
+alert("GG")
