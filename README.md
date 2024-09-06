b872ba06e5134519d73c6c0db638c607812b4318
imadghali01 <imadghali01@gmail.com>
2024-09-06 16:28:22 +0200

    exo1-part11

--- README.md
+++ README.md
@@ -3,21 +3,6 @@
  
 ## part 11
 
-Let's try another thing... Write a code that will log to the console only the even numbers untill 20.
 
-The goal here is to see that the increment expression doesn't have to be necessarely `i++`
-
-Attention : the 0 should no be counted.
-
-The result in your console should be :
-
-`2 4 6 8 10 12 14 16 18 20
-`
-
-Now, do the same thing for odd numbers untille 20.
-
-The result in your console should be :
-
-`1 3 5 7 9 11 13 15 17 19
 `
 
--- app.js
+++ app.js
@@ -1,24 +1,15 @@
-/*Let's try another thing... Write a code that will log to the console only the even numbers untill 20.
-
-The goal here is to see that the increment expression doesn't have to be necessarely `i++`
-
-Attention : the 0 should no be counted.
-
-The result in your console should be :
-
-`2 4 6 8 10 12 14 16 18 20
-`
-
-Now, do the same thing for odd numbers untille 20.
-
-The result in your console should be :
-
-`1 3 5 7 9 11 13 15 17 19
-`
-
-*/
-for( let i=1 ; i < 20 ; i++){
-    if((i % 2) != 0){
+for (let i = 100 ; i > 0; i--) {
+    if((i % 10) == 0){
         console.log(i);
     }
+    
+}
+for (let i = 25 ; i >= 0; i--) {
+    if((i % 5) == 0){
+        console.log(i);
+    }
+    
+}
+for (let i = 1 ; i <= 3; i++) {
+    console.log(10 ** i);
 }
