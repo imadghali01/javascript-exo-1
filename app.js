61cbeb76c40404e24190fa7eebf15ad60069b2d1
imadghali01 <imadghali01@gmail.com>
2024-09-09 15:42:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,3 +6,20 @@
     printAge should console.log (firstName) (lastName) is (age) years old
     Play in your console to print his age and then increment his age.
 */
+
+const user = {
+    firstName : "manu",
+    lastName : "lebanner",
+    age : "50",
+    printAge : function(){
+        return console.log(this.firstName +" "+this.lastName + " is " + this.age);
+    },
+    aYearHasPast : function(){
+        this.age++;
+    }
+}
+
+
+user.printAge();
+user.aYearHasPast();
+user.printAge();
